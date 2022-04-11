f0dbcb6e20ea91ed1d71a9d56dd5f77b03b944d5
jenzhng <zhongjenn@gmail.com>
2022-04-11 14:33:52 -0400

    changes to styling

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,11 +6,14 @@ import AnchorLink from "react-anchor-link-smooth-scroll";
 
 export const About = () => {
   return (
-     <div className="about-section" id="about">
-		  <h1 className="font-weight-bold"> About </h1>
+  <>
+  <h1 className="font-weight-bold"> About </h1>
+     <div className="collection" id="about">
+		  
 		  <p className="normal">  We're a local t-shirt printing shop ready to accept custom print orders. Feel free to send us your shirt and embroidery designs for a free quote. </p>
 
         </div>
+		</>
   );
 };
 
